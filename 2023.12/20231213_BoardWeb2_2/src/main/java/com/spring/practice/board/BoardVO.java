18bfacd1e856683c90677ec0b13f273d81017b68
carelesspark <kiae1234@naver.com>
2023-12-13 18:48:47 +0900

    # 2023.12.13 Study ) MVC2 패턴에 Service추가해 DAO 사용(Controller -> Service -> DAO)

---
+++ 2023.12/20231213_BoardWeb2_2/src/main/java/com/spring/practice/board/BoardVO.java
@@ -0,0 +1,17 @@
+package com.spring.practice.board;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private int seq;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regDate;
+	private int cnt;
+	private String searchCondition;
+	private String searchKeyword;
+}
---
+++ 2023.12/20231213_BoardWeb2_2/src/main/java/com/spring/practice/common/AroundAdvice.java
@@ -0,0 +1,17 @@
+package com.spring.practice.common;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+public class AroundAdvice {
+	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
+		// before
+		System.out.println("[Before]: 비즈니스 메소드 수행 전에 처리할 내용...");
+		Object returnObj = pjp.proceed();
+		
+		// after
+		System.out.println("[After]: 비즈니스 메소드 수행 후에 처리할 내용...");
+		
+		
+		return returnObj;
+	}
+}
